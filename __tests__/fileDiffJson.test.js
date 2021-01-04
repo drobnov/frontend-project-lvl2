ad47eb079903d42576bc0d2c10722c3e7c64567b
Pavlo Drobnov <drobnovpavlo89@gmail.com>
2021-01-04 19:00:57 +0200

    refactoring

--- __tests__/fileDiffJson.test.js
+++ __tests__/fileDiffJson.test.js
@@ -1,21 +1,16 @@
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
+import fs from 'fs';
 import fileDiff from '..';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
-const formats = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
 
 test('fileDiff', () => {
+  const testFilePath = getFixturePath('testFileJson');
+  const testFile = fs.readFileSync(testFilePath, 'utf-8');
   const file1Path = getFixturePath('file1.json');
   const file2Path = getFixturePath('file2.json');
-  expect(fileDiff(file1Path, file2Path)).toEqual(formats);
+  expect(fileDiff(file1Path, file2Path)).toEqual(testFile);
 });
