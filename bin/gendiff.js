81a8af65146279890a2c3526e275675a58e854fc
Pavlo Drobnov <drobnovpavlo89@gmail.com>
2021-01-09 13:48:15 +0200

    add func "stylish", default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import fileDiff from '../index.js';
+import { fileDiff, stylish } from '../index.js';
 
 const program = new Command();
 
 program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    const result = fileDiff(filepath1, filepath2);
-    return console.log(result);
-  })
   .description('Compares two configuration files and shows a difference')
-  .option('-f, --format [type]', 'output format');
-
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    const diff = fileDiff(filepath1, filepath2);
+    const format = stylish(diff);
+    return console.log(format);
+  });
 program.parse(process.argv);
 
 if (!program.args.length) program.help();
