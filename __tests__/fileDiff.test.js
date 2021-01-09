6c6d829156cc687f3cbbf7289b56ec081b866a44
Pavlo Drobnov <drobnovpavlo89@gmail.com>
2021-01-09 13:46:17 +0200

    add test file_tree format

--- __tests__/fileDiff.test.js
+++ __tests__/fileDiff.test.js
@@ -1,24 +1,26 @@
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import fs from 'fs';
-import fileDiff from '..';
+import { fileDiff, stylish } from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 
-test('fileDiffJson', () => {
-  const testFilePath = getFixturePath('testFile');
+test('file_tree_Diff_json', () => {
+  const testFilePath = getFixturePath('testFile_tree.txt');
   const testFile = fs.readFileSync(testFilePath, 'utf-8');
-  const file1Path = getFixturePath('file1.json');
-  const file2Path = getFixturePath('file2.json');
-  expect(fileDiff(file1Path, file2Path)).toEqual(testFile);
+  const file1Path = getFixturePath('file_tree1.json');
+  const file2Path = getFixturePath('file_tree2.json');
+  const diff = fileDiff(file1Path, file2Path);
+  expect(stylish(diff)).toEqual(testFile);
 });
 
-test('fileDiffYml', () => {
-  const testFilePath = getFixturePath('testFile');
+test('file_tree_Diff_yml', () => {
+  const testFilePath = getFixturePath('testFile_tree.txt');
   const testFile = fs.readFileSync(testFilePath, 'utf-8');
-  const file1Path = getFixturePath('file1.yml');
-  const file2Path = getFixturePath('file2.yml');
-  expect(fileDiff(file1Path, file2Path)).toEqual(testFile);
+  const file1Path = getFixturePath('file_tree1.yml');
+  const file2Path = getFixturePath('file_tree2.yml');
+  const diff = fileDiff(file1Path, file2Path);
+  expect(stylish(diff)).toEqual(testFile);
 });
