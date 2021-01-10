077d9bd49cc6a8a6dbcc11c70cd618c63a74cddc
Pavlo Drobnov <drobnovpavlo89@gmail.com>
2021-01-10 12:08:44 +0200

    refactoring

--- src/fileDiff.js
+++ src/fileDiff.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import parsers from './parsers/parsers.js';
+import formatters from './formatters/index.js';
 
-const sortCb = (a, b) => { // call back for sort function
+const sortCb = (a, b) => {
   if (a.key > b.key) {
     return 1;
   }
@@ -11,7 +12,7 @@ const sortCb = (a, b) => { // call back for sort function
   return 0;
 };
 
-const addDeph = (data, deph) => { // function for calculating the depth of the element
+const dephElement = (data, deph) => {
   if (!_.isPlainObject(data)) {
     return data;
   }
@@ -19,7 +20,7 @@ const addDeph = (data, deph) => { // function for calculating the depth of the e
   const keys = Object.keys(data);
   keys.forEach((key) => {
     a = [...a, {
-      key, value: addDeph(data[key], deph + 1), deph: deph + 1, type: null,
+      key, value: dephElement(data[key], deph + 1), deph: deph + 1, type: null,
     }];
   });
   return a;
@@ -39,7 +40,7 @@ const fileDiff = (filePath1, filePath2) => {
     const keysAdd = [...keys2].filter((key) => !keys1.includes(key));
     keysAdd.forEach((key) => {
       result = [...result, {
-        key, value: addDeph(objectFile2[key], deph), type: 'add', deph,
+        key, value: dephElement(objectFile2[key], deph), type: 'add', deph, sort: 'no',
       }];
       result.sort(sortCb);
     });
@@ -47,22 +48,22 @@ const fileDiff = (filePath1, filePath2) => {
     keys1.forEach((key) => {
       if (_.isEqual(objectFile1[key], objectFile2[key])) { // add the same data
         result = [...result, {
-          key, value: addDeph(objectFile1[key], deph), type: null, deph,
+          key, value: dephElement(objectFile1[key], deph), type: null, deph,
         }];
         result.sort(sortCb);
         // same keys, different data in the keys
       } else if (keys2.includes(key) && (typeof objectFile1[key] !== typeof objectFile2[key]
         || (!_.isPlainObject(objectFile1[key]) || !_.isPlainObject(objectFile2[key])))) {
         result = [...result, {
-          key, value: addDeph(objectFile1[key], deph), type: 'delete', deph,
+          key, value: dephElement(objectFile1[key], deph), type: 'delete', deph,
         }];
         result = [...result, {
-          key, value: addDeph(objectFile2[key], deph), type: 'add', deph,
+          key, value: dephElement(objectFile2[key], deph), type: 'add', deph,
         }];
         result.sort(sortCb);
       } else if (!keys2.includes(key)) { // keys are not in the second file
         result = [...result, {
-          key, value: addDeph(objectFile1[key], deph), type: 'delete', deph,
+          key, value: dephElement(objectFile1[key], deph), type: 'delete', deph, sort: 'no',
         }];
         result.sort(sortCb);
         // identical keys are the data type that are the object
@@ -78,33 +79,10 @@ const fileDiff = (filePath1, filePath2) => {
   return genDiffs(parseFile1, parseFile2);
 };
 
-const stylish = (diffs) => {
-  const separ = (type) => {
-    switch (type) {
-      case 'add':
-        return '+';
-      case 'delete':
-        return '-';
-      default:
-        return ' ';
-    }
-  };
-
-  const formater = (diff, index = 0) => {
-    let str = [];
-
-    diff.forEach(({
-      key, value, type, deph,
-    }) => {
-      if (!Array.isArray(value)) {
-        str = [...str, `${' '.repeat(deph + 5 + (index - 3))}${separ(type)} ${key}: ${value}`];
-      } else {
-        str = [...str, `${' '.repeat(deph + 2 + index)}${separ(type)} ${key}: {\n${formater(value, index + 3)}\n${' '.repeat(deph + 4 + index)}}`];
-      }
-    });
-    return str.join('\n');
-  };
-  return `{\n${formater(diffs)}\n}\n`;
+const fileDiffFormat = (filePath1, filePath2, formatName = 'stylish') => {
+  const fileDiffStructure = fileDiff(filePath1, filePath2);
+  const formatDiff = formatters(formatName, fileDiffStructure);
+  return formatDiff;
 };
 
-export { fileDiff, stylish };
+export default fileDiffFormat;
