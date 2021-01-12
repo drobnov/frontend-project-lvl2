2b780243b7677d1726ad3e57ca73f6b14c0c0f46
Pavlo <53899731+drobnov@users.noreply.github.com>
2021-01-13 00:36:48 +0200

    refactoring

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -5,26 +5,20 @@ export const plainStructure = (diff) => {
 
   const iter = (diffObject, path) => {
     diffObject.forEach(({
-      key, value, type, sort, deph,
+      key, value, type, sort,
     }) => {
       if (!Array.isArray(value) && type === null) {
         result = [...result];
-      } else if ((sort === 'no' || sort === 'no analyze') && type !== null) {
-        if (!Array.isArray(value)) {
-          result = [...result, {
-            key, value, type, path: [...path, key].join('.'), deph,
-          }];
-        } else {
-          result = [...result, {
-            key, children: value, type, path: [...path, key].join('.'), deph,
-          }];
-        }
-      } else if (type !== null && (sort !== 'no' || sort !== 'no analyze')) {
+      } else if (sort === 'no' && type !== null) {
+        result = [...result, {
+          key, value, type, path: [...path, key].join('.'),
+        }];
+      } else if (type !== null && sort !== 'no') {
         const diffAddDel = diffObject.filter((a) => a.key === key);
         const added = _.find(diffAddDel, { type: 'add' });
         const removed = _.find(diffAddDel, { type: 'delete' });
         result = [...result, {
-          key, value: { delete: removed.value, add: added.value }, type: 'updated', path: [...path, key].join('.'), deph,
+          key, value: { delete: removed.value, add: added.value }, type: 'updated', path: [...path, key].join('.'),
         }];
       } else {
         iter(value, [...path, key]);
@@ -44,18 +38,16 @@ const constructionValue = (value) => {
   return value;
 };
 
-const valueOrChildren = (value, children) => (value !== undefined ? value : children);
-
 const buildString = (structure) => {
   const result = [];
   structure.forEach(({
-    value, type, path, children,
+    value, type, path,
   }) => {
     if (type === 'delete') {
       const str = `Property '${path}' was removed`;
       result.push(str);
     } else if (type === 'add') {
-      const str = `Property '${path}' was added with value: ${constructionValue(valueOrChildren(value, children))}`;
+      const str = `Property '${path}' was added with value: ${constructionValue(value)}`;
       result.push(str);
     } else if (type === 'updated') {
       const str = `Property '${path}' was updated. From ${constructionValue(value.delete)} to ${constructionValue(value.add)}`;
