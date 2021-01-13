d0a1ff67e7a6e65fc6f4070f1e9cf2c4d1ab5ed8
Pavlo Drobnov <drobnovpavlo89@gmail.com>
2021-01-13 19:57:59 +0200

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,22 +1,24 @@
-const separ = (type, sort) => {
-  if (type === 'add' && sort !== 'no analyze') {
-    return '+';
-  } if (type === 'delete' && sort !== 'no analyze') {
-    return '-';
+const separ = (type) => {
+  switch (type) {
+    case 'add':
+      return '+';
+    case 'delete':
+      return '-';
+    default:
+      return ' ';
   }
-  return ' ';
 };
 
 const stylish = (diffs) => {
   const formater = (diff, index = 0) => {
     let str = [];
     diff.forEach(({
-      key, value, type, deph, sort,
+      key, value, type, deph,
     }) => {
       if (!Array.isArray(value)) {
-        str = [...str, `${' '.repeat(deph + 5 + (index - 3))}${separ(type, sort)} ${key}: ${value}`];
+        str = [...str, `${' '.repeat(deph + 5 + (index - 3))}${separ(type)} ${key}: ${value}`];
       } else {
-        str = [...str, `${' '.repeat(deph + 2 + index)}${separ(type, sort)} ${key}: {\n${formater(value, index + 3)}\n${' '.repeat(deph + 4 + index)}}`];
+        str = [...str, `${' '.repeat(deph + 2 + index)}${separ(type)} ${key}: {\n${formater(value, index + 3)}\n${' '.repeat(deph + 4 + index)}}`];
       }
     });
     return str.join('\n');
